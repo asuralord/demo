3025e0e7b75620027ee78a886ffee9a79ffbba93
asuralord <asuralord@naver.com>
2023-02-28 12:42:38 +0900

    First commit

---
+++ src/main/java/com/democommunity/demo/controller/FrontController.java
@@ -0,0 +1,18 @@
+package com.democommunity.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fronts")
+public class FrontController {
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Long> test(@PathVariable Long id) {
+        System.out.println("!!!!!");
+        return ResponseEntity.ok().body(id);
+    }
+}
